b0e019c1e29567f7213351dab139d89bcb6a6b4b
Nithish12Krish <nithish1281999@gmail.com>
2021-02-24 21:28:08 +0530

    Rank component added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Particles from 'react-particles-js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 import './App.css';
 
+ const particlesOption={
+  particles: {
+    number:{
+      value:30,
+      density: {
+        enable:true,
+        value_area:300
+      }
+    }
+  }
+}
 function App() {
   return (
     <div className="App">
+       <Particles  className='particles'
+              params={particlesOption}
+            />
       <Navigation />
       <Logo />
+      <Rank /> 
       <ImageLinkForm />
-      {/* </Rank> */}
+      
       
     </div>
   );
